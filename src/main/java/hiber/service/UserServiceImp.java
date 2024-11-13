7eb5725e2663f935dad8cb9a5e11f5ef47c533d5
Aleksandr Zakharchuk <allexss86@gmail.com>
2024-11-13 21:17:59 +0300

    Решенная задача

--- src/main/java/hiber/service/UserServiceImp.java
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -11,19 +11,29 @@ import java.util.List;
 @Service
 public class UserServiceImp implements UserService {
 
-   @Autowired
-   private UserDao userDao;
+    @Autowired
+    private UserDao userDao;
 
-   @Transactional
-   @Override
-   public void add(User user) {
-      userDao.add(user);
-   }
+    @Transactional
+    @Override
+    public void add(User user) {
+        userDao.add(user);
+    }
 
-   @Transactional(readOnly = true)
-   @Override
-   public List<User> listUsers() {
-      return userDao.listUsers();
-   }
+    @Transactional(readOnly = true)
+    @Override
+    public List<User> listUsers() {
+        return userDao.listUsers();
+    }
+
+    @Transactional
+    @Override
+    public List<User> getUserByCarModelAndSeries() {
+        List<User> users = userDao.getUserByCarModelAndSeries();
+        if (users == null) {
+            System.out.println("Нет авто такой модели либо серии");
+        }
+        return users;
+    }
 
 }
